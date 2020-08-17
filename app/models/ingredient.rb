448fa03d650998224f075d7ad253bc1dc918ab70
Emily Rockarts <em.rockarts@gmail.com>
2020-08-16 21:20:56 -0400

    rails new

--- app/models/ingredient.rb
+++
@@ -1,6 +0,0 @@
-class Ingredient < ApplicationRecord
-  has_many :doses
-  has_many :cocktails, through: :doses
-
-  validates :name, uniqueness: true, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,4 @@ Rails.application.routes.draw do
   # get 'cocktails/new', to: 'cocktails#new', as: 'new_cocktail'
   # post 'cocktails/', to: 'cocktails#create', as:
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:new, :create]
-      # resources :ingredients, only:
-  end
-  resources :doses, only: [:destroy]
 end
